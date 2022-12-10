f01049bf810e326b6d54b70c36e484452167cfbc
Blazej Sewera <code@sewera.dev>
2022-12-10 18:34:22 +0100

    feat: add swisstypefaces fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      geometric: [
+        'Euclid, Futura, "Trebuchet MS", Arial, sans-serif',
+        { fontFeatureSettings: '"kern"' },
+      ],
+      sans: [
+        '"Suisse Int\'l", -apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif',
+        { fontFeatureSettings: '"kern"' },
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
 }
