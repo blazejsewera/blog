d5a2f2211fb7963fecca7de10411cff5fe2ae91b
Blazej Sewera <code@sewera.dev>
2022-12-23 23:09:38 +0100

    refactor: remove tailwind and components

--- src/components/heading/H2.tsx
+++
@@ -1,7 +0,0 @@
-import type { Component } from 'solid-js'
-import { cx } from '../util/classes'
-import { BIG_TITLE_FONT, HeadingProps } from './common'
-
-export const H2: Component<HeadingProps> = ({ children }) => (
-  <h2 class={cx(BIG_TITLE_FONT, 'text-2xl')}>{children}</h2>
-)
--- src/components/heading/common.ts
+++
@@ -1,6 +0,0 @@
-export const BIG_TITLE_FONT = 'font-geometric'
-export const SMALL_TITLE_FONT = 'font-sans'
-
-export interface HeadingProps {
-  children: string | Element
-}
