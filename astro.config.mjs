ed465414f11317a7c2e8670aeb2c526536f82f98
Blazej Sewera <code@sewera.dev>
2023-01-11 14:48:27 +0100

    chore: include draft pages in dist build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config';
-import solidJs from '@astrojs/solid-js';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import solidJs from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
   markdown: {
+    drafts: true,
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: 'github-light'
-    }
-  }
-});
+      theme: 'github-light',
+    },
+  },
+})
