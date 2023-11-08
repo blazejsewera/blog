580dc780c3b7d6d7d0d15d2db4c353eea8af2567
Blazej Sewera <code@sewera.dev>
2023-11-08 20:06:33 +0100

    test: update generics in assertion functions

--- go/internal/assert/standard.go
+++ go/internal/assert/standard.go
@@ -10,16 +10,12 @@ func Equal[T any](t testing.TB, expected T, actual T, errorMsg ...any) bool {
 	return tt.Equal(t, expected, actual, errorMsg...)
 }
 
-func Zero[T any](t testing.TB, value T, errorMsg ...any) bool {
+func Zero(t testing.TB, value any, errorMsg ...any) bool {
 	t.Helper()
 	return tt.Zero(t, value, errorMsg...)
 }
 
-func Len[T ~[]any | ~[...]any | ~*[...]any | ~map[any]any | string | chan any](
-	t testing.TB,
-	expectedLen int,
-	value T,
-	errorMsg ...any) bool {
+func Len(t testing.TB, expectedLen int, value any, errorMsg ...any) bool {
 	t.Helper()
 	return tt.Len(t, value, expectedLen, errorMsg...)
 }
